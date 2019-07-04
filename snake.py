e1b6152bd659b47a5ef8706dabb50c8144e0730a
mhtsort <mhtsort@gmail.com>
2019-07-04 04:32:01 +0300

    Working Example with basic printing of Board

--- snake.py
+++ snake.py
@@ -2,17 +2,21 @@
 import tkinter as tk
 
 class App():
-    def __init__(self,root,width=600,heigth=400):
-        self.width=width
-        self.heigth=heigth
+    def __init__(self,root,width=600,height=400):
         self.root=root
-        self.root.geometry("{}x{}".format(self.width,self.heigth))
+        #Application width and hegth
+        self.width=width
+        self.height=height
+        self.root.geometry("{}x{}".format(self.width,self.height))
+        #Create a Frame on top for buttons and info
         self.frame=tk.Frame (self.root)
         self.frame.pack(fill="x")
+        #Create restart button
         self.restart=tk.Button(self.frame,text="restart",command=self.restartgame)
-        self.restart.pack()
-        self.debug=tk.Button(self.frame,text="restart",command=self.printstatus)
-        self.debug.pack()
+        self.restart.pack(side="left")
+        #Create status label and button for development info
+        self.debug=tk.Button(self.frame,text="print status",command=self.printstatus)
+        self.debug.pack(side="left")
         self.statuslabel=tk.Label(self.frame,text="Status",background="lightgreen")
         self.statuslabel.pack(side="bottom")
         self.board=Board(10,10)
@@ -21,6 +25,7 @@ class App():
         pass
     def printstatus(self):
         self.board._printboard()
+        self.statuslabel.configure(text="TODO")
 class Board():
     def createtiles(self,rows,cols):
         '''
@@ -36,13 +41,15 @@ class Board():
         '''
         Pretty prints matrix reprisenting board for DEBUGING 
         '''
+        boardasstring=""
         for i in self.board:
             print(i)
-        
+            boardasstring+="\n {}".format(i)
+        print("___________")
+        return boardasstring
     def __init__(self,horizontal,vertical):
-        self.horizontal_tiles_num=Null
-        self.vertical_tiles_num=Null
-        self.tiles=[(i,j) for i in horizontal_tiles ]
+        self.horizontal_tiles_num=horizontal
+        self.vertical_tiles_num=vertical
         self.board=self.createtiles(horizontal,vertical)
         
 
